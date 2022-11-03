9fe7d580e0ee8081bdfdd179a128378e664e9bba
mgrouch <mgrushinskiy@gmail.com>
2022-11-03 10:39:13 -0400

    test data

--- wave_height_from_IMU.py
+++ wave_height_from_IMU.py
@@ -63,29 +63,29 @@ n_dim_obs = 2
 filtered_state_means = np.zeros((n_timesteps, n_dim_state))
 filtered_state_covariances = np.zeros((n_timesteps, n_dim_state, n_dim_state))
 
-kf = KalmanFilter(transition_matrices = F,
-                  observation_matrices = H,
-                  transition_covariance = Q,
-                  observation_covariance = R,
-                  initial_state_mean = X0,
-                  initial_state_covariance = P0,
-                  n_dim_obs = 2)
-
-# iterative estimation for each new measurement
-for t in range(n_timesteps):
-    if t == 0:
-        filtered_state_means[t] = X0
-        filtered_state_covariances[t] = P0
-    else:
-        observation = [0,
-                       AccX_Value[t, 0]]
-        filtered_state_means[t], filtered_state_covariances[t] = (
-            kf.filter_update(
-                filtered_state_mean = filtered_state_means[t-1],
-                filtered_state_covariance = filtered_state_covariances[t-1],
-                observation = observation
-            )
-        )
+# kf = KalmanFilter(transition_matrices = F,
+#                   observation_matrices = H,
+#                   transition_covariance = Q,
+#                   observation_covariance = R,
+#                   initial_state_mean = X0,
+#                   initial_state_covariance = P0,
+#                   n_dim_obs = 2)
+#
+# # iterative estimation for each new measurement
+# for t in range(n_timesteps):
+#     if t == 0:
+#         filtered_state_means[t] = X0
+#         filtered_state_covariances[t] = P0
+#     else:
+#         observation = [0,
+#                        AccX_Value[t, 0]]
+#         filtered_state_means[t], filtered_state_covariances[t] = (
+#             kf.filter_update(
+#                 filtered_state_mean = filtered_state_means[t-1],
+#                 filtered_state_covariance = filtered_state_covariances[t-1],
+#                 observation = observation
+#             )
+#         )
 
 
 f, axarr = plt.subplots(3, sharex=True)
@@ -95,20 +95,20 @@ axarr[0].plot(Time, filtered_state_means[:, 3], "r-", label="Estimated AccX")
 axarr[0].set_title('Acceleration X')
 axarr[0].grid()
 axarr[0].legend()
-axarr[0].set_ylim([-4, 4])
+axarr[0].set_ylim([-0.3, 0.3])
 
 axarr[1].plot(Time, RefVelX, label="Reference VelX")
 axarr[1].plot(Time, filtered_state_means[:, 2], "r-", label="Estimated VelX")
 axarr[1].set_title('Velocity X')
 axarr[1].grid()
 axarr[1].legend()
-axarr[1].set_ylim([-1, 20])
+axarr[1].set_ylim([-0.4, 0.4])
 
 axarr[2].plot(Time, RefPosX, label="Reference PosX")
 axarr[2].plot(Time, filtered_state_means[:, 1], "r-", label="Estimated PosX")
 axarr[2].set_title('Position X')
 axarr[2].grid()
 axarr[2].legend()
-axarr[2].set_ylim([-10, 1000])
+axarr[2].set_ylim([-2, 2])
 
 plt.show()
