472fdaa214e12fbf42fe5378cf593e61d788084c
mgrouch <mgrushinskiy@gmail.com>
2022-11-05 22:35:32 -0400

    upd

--- trochoidal_wave.py
+++ trochoidal_wave.py
@@ -2,18 +2,17 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-g = 9.806
+g = 9.806  # Gravitational G (m/s^2)
 
-b = -1
-L = 10
+b = -1     # Rotation center in Y axis (m)
+L = 10     # Wave length (m)
 
-k = 2 * np.pi / L
-c = np.sqrt(g / k)
-u = np.sqrt(g * L / (2 * np.pi))
-H = np.exp(k * b) / k
-T = L / c
+k = 2 * np.pi / L                  # Wave number (1/m)
+c = np.sqrt(g / k)                 # Speed in X direction  m/s
+H = np.exp(k * b) / k              # Wave height (m)
+T = L / c                          # Wave period (s)
 
-print(f'Length: {L}, Height: {H}, Period: {T}, Speed: {u}')
+print(f'Length: {L}, Height: {H}, Period: {T}, Speed: {c}')
 
 n_timesteps = 1000
 dt = 0.01
@@ -24,10 +23,8 @@ y_val = np.zeros(n_timesteps)
 for ii in range(n_timesteps):
     t = ii * dt
     x = H * np.sin(k * c * t)
-    #x = ii * dt
-    #t = np.arcsin(x * k / np.exp(k * b)) / (k * c) + x
     y = - H * np.cos(k * c * t)
-    x_val[ii] = x + t * u
+    x_val[ii] = x + t * c
     y_val[ii] = y
 
 plt.plot(x_val, y_val, "r-")
