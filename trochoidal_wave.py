5b8fabd1f7b1e090d1a37efda821f66eaaaf051c
mgrouch <mgrushinskiy@gmail.com>
2022-11-05 22:21:19 -0400

    upd

--- trochoidal_wave.py
+++ trochoidal_wave.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 
 g = 9.806
 
-a = 0
 b = -1
 L = 10
 
 k = 2 * np.pi / L
 c = np.sqrt(g / k)
 u = np.sqrt(g * L / (2 * np.pi))
+H = np.exp(k * b) / k
+T = L / c
+
+print(f'Length: {L}, Height: {H}, Period: {T}, Speed: {u}')
 
 n_timesteps = 1000
 dt = 0.01
@@ -20,10 +23,10 @@ y_val = np.zeros((n_timesteps))
 
 for ii in range(n_timesteps):
     t = ii * dt
-    x = a + (np.exp(k * b) / k * np.sin(k * (a + c * t)))
-    # x = ii * dt
-    # t = ((np.arcsin((x - a) * k / np.exp(k * b)) / k) - a) / c
-    y = - np.exp(k * b) / k * np.cos(k * (a + c * t))
+    x = H * np.sin(k * c * t)
+    #x = ii * dt
+    #t = np.arcsin(x * k / np.exp(k * b)) / (k * c) + x
+    y = - H * np.cos(k * c * t)
     times[ii] = x + t * u
     y_val[ii] = y
 
