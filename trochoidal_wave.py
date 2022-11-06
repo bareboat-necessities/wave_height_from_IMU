ce61a43710dbe0717718d5c8772c04e6a06d77f4
mgrouch <mgrushinskiy@gmail.com>
2022-11-06 00:35:26 -0400

    upd

--- trochoidal_wave.py
+++ trochoidal_wave.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 
 g = 9.806  # Gravitational G (m/s^2)
 
-b = -1  # Rotation center in Y axis (m)
-L = 10  # Wave length (m)
+b = -1.5    # Rotation center in Y axis (m)
+L = 15       # Wave length (m)
 
-k = 2 * np.pi / L  # Wave number (1/m)
-c = np.sqrt(g / k)  # Speed in X direction  m/s
-H = np.exp(k * b) / k  # Wave height (m)
-T = L / c  # Wave period (s)
+k = 2 * np.pi / L                  # Wave number (1/m)
+c = np.sqrt(g / k)                 # Speed in X direction  m/s
+H = np.exp(k * b) / k              # Wave height (m)
+T = L / c                          # Wave period (s)
 
 print(f'Length: {L}, Height: {H}, Period: {T}, Speed: {c}')
 
-n_timesteps = 4000
+n_timesteps = 2000
 dt = 0.01
 
-time_val = np.zeros(n_timesteps)
+x_val = np.zeros(n_timesteps)
 y_val = np.zeros(n_timesteps)
 
 
@@ -26,14 +26,12 @@ def triangle_wave(w, a, p):
 
 
 for ii in range(n_timesteps):
-    tx = ii * dt
-    tw = triangle_wave(tx, H / 2, L * 4)
-    t = np.arcsin(tw) / (k * c)
+    t = ii * dt
+    x = H * np.sin(k * c * t)
     y = - H * np.cos(k * c * t)
-    time_val[ii] = tx
+    x_val[ii] = x/c + t
     y_val[ii] = y
-    print(f'{time_val[ii]}   {y_val[ii]}')
 
-plt.plot(time_val, y_val, "r-")
+plt.plot(x_val, y_val, "r-")
 plt.grid()
 plt.show()
