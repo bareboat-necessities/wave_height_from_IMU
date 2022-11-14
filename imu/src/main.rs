08774103827563fadcf2d5ac02d700e4876b0b3b
mgrouch <mgrushinskiy@gmail.com>
2022-11-14 14:30:22 -0500

    roll, pitch, yaw

--- imu/src/main.rs
+++ imu/src/main.rs
@@ -11,6 +11,10 @@ use hal::Delay;
 use hal::I2cdev;
 use mpu9250::{Mpu9250, MargMeasurements};
 
+use ahrs::{Ahrs, Madgwick};
+use nalgebra::Vector3;
+use std::f64;
+
 fn main() -> io::Result<()> {
     let i2c = I2cdev::new("/dev/i2c-1").expect("unable to open /dev/i2c-1");
 
@@ -27,11 +31,28 @@ fn main() -> io::Result<()> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
     writeln!(&mut stdout,
-             "   Accel XYZ(m/s^2)  |   Gyro XYZ (rad/s)  |  Mag Field XYZ(uT)  | Temp (C)")?;
+             "   Accel XYZ(m/s^2)  |   Gyro XYZ (rad/s)  |  Mag Field XYZ(uT)  | Temp (C) | Roll | Pitch | Yaw")?;
     loop {
         let all: MargMeasurements<[f32; 3]> = mpu9250.all().expect("unable to read from MPU!");
+        let mut ahrs = Madgwick::default();
+
+        // Obtain sensor values from a source
+        let gyroscope = Vector3::new(all.gyro[0], all.gyro[1], all.gyro[2]);
+        let accelerometer = Vector3::new(all.accel[0], all.accel[1], all.accel[2]);
+        let magnetometer = Vector3::new(all.mag[0], all.mag[1], all.mag[2]);
+
+        // Run inputs through AHRS filter (gyroscope must be radians/s)
+        let quat = ahrs
+            .update(
+                &(gyroscope * (f64::consts::PI / 180.0)),
+                &accelerometer,
+                &magnetometer,
+            )
+            .unwrap();
+        let (roll, pitch, yaw) = quat.euler_angles();
+
         write!(&mut stdout,
-               "\r{:>6.2} {:>6.2} {:>6.2} |{:>6.1} {:>6.1} {:>6.1} |{:>6.1} {:>6.1} {:>6.1} | {:>4.1} ",
+               "\r{:>6.2} {:>6.2} {:>6.2} |{:>6.1} {:>6.1} {:>6.1} |{:>6.1} {:>6.1} {:>6.1} | {:>4.1} | {:>4.1} | {:>4.1} | {:>4.1}",
                all.accel[0],
                all.accel[1],
                all.accel[2],
@@ -41,7 +62,11 @@ fn main() -> io::Result<()> {
                all.mag[0],
                all.mag[1],
                all.mag[2],
-               all.temp)?;
+               all.temp,
+               roll,
+               pitch,
+               yaw
+        )?;
         stdout.flush()?;
         thread::sleep(Duration::from_micros(100000));
     }
