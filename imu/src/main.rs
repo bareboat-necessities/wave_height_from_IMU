775cca90a2ef643a37a6461252bf187346b6c989
mgrouch <mgrushinskiy@gmail.com>
2022-11-14 23:51:38 -0500

    vert_pos

--- imu/src/main.rs
+++ imu/src/main.rs
@@ -90,6 +90,8 @@ fn main() -> io::Result<()> {
     writeln!(&mut stdout,
              "   Accel XYZ(m/s^2)  |   Gyro XYZ (rad/s)  |  Mag Field XYZ(uT)  | Temp (C) | Roll   | Pitch  | Yaw    | Vert Acc - g (m/s^2) | VPos(m)")?;
 
+    let mut vert_pos = 0.0;
+    let g = 9.806;
     let mut t: usize = 0;
     loop {
         let all: MargMeasurements<[f32; 3]> = mpu9250.all().expect("unable to read from MPU!");
@@ -112,7 +114,6 @@ fn main() -> io::Result<()> {
         let rotated_acc= quat.transform_vector(
             &Vector3::new(all.accel[0] as f64, all.accel[1] as f64, all.accel[2] as f64));
 
-        let g = 9.806;
         let vert_acc_minus_g = rotated_acc[2] - g;
 
         t = t + 1;
@@ -120,7 +121,6 @@ fn main() -> io::Result<()> {
             acc_mean.add_sample(vert_acc_minus_g);
         }
 
-        let mut vert_pos = 0.0;
         if t >= (2 * SAMPLES) {
             filtered = update_step(&kf, &predicted, &Vector::new(vec![0.0]));
             filtered.x = &filtered.x + &b * (vert_acc_minus_g - acc_mean.get_average());
