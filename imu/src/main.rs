e4d55d7fe35cbec3b291b2c3a8f58fcb5a2d7d6b
mgrouch <mgrushinskiy@gmail.com>
2022-11-15 23:32:00 -0500

    vert_pos

--- imu/src/main.rs
+++ imu/src/main.rs
@@ -40,7 +40,7 @@ fn main() -> io::Result<()> {
     println!("AK8963 who_am_I       |     0x{:x}", mag_who_am_i);
     assert_eq!(who_am_i, 0x71);
 
-    mpu9250.accel_data_rate(AccelDataRate::DlpfConf(Dlpf::_2)).expect("Err setting rate");
+    mpu9250.accel_data_rate(AccelDataRate::DlpfConf(Dlpf::_3)).expect("Err setting rate");
 
     println!("accel resolution      | {:>8.5}", mpu9250.accel_resolution());
     println!("gyro resolution       | {:>8.5}", mpu9250.gyro_resolution());
@@ -56,7 +56,7 @@ fn main() -> io::Result<()> {
     let mut stdout = stdout.lock();
     const WAIT_SEC: f64 = 0.1;
 
-    const IMU_SAMPLE_SEC: f64 = 0.01;
+    const IMU_SAMPLE_SEC: f64 = 0.02;
 
     let mut ahrs = Madgwick::new_with_quat(
         IMU_SAMPLE_SEC,
