3bccc589bd75cf6ff7f92def2ce6e173db2f53ef
mgrouch <mgrushinskiy@gmail.com>
2022-11-15 21:38:47 -0500

    vert_pos

--- imu/src/main.rs
+++ imu/src/main.rs
@@ -152,8 +152,9 @@ fn main() -> io::Result<()> {
                     write!(&mut stdout, "mag field XYZ (uT)    | {:>8.3} {:>8.3} {:>8.3}\n", magnetometer[0], magnetometer[1], magnetometer[2])?;
                     write!(&mut stdout, "roll/pitch/yaw (deg)  | {:>8.3} {:>8.3} {:>8.3}\n", roll * 180.0 / f64::consts::PI, pitch * 180.0 / f64::consts::PI, yaw * 180.0 / f64::consts::PI)?;
                     write!(&mut stdout, "temp (C)              | {:>8.3}\n", all.temp)?;
+                    write!(&mut stdout, "time elapsed          | {:?}                   \n", t.elapsed())?;
                     stdout.flush()?;
-                    write!(&mut stdout, "{}", move_up_csi_sequence(5))?;
+                    write!(&mut stdout, "{}", move_up_csi_sequence(6))?;
 
                     thread::sleep(Duration::from_micros((IMU_SAMPLE_SEC * 1000000.0) as u64));
                     //    .checked_sub(t.elapsed()).expect("Err time subtract"));
