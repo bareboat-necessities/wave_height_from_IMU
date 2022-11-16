1ad3b10430de304e96b8f0af2aa6b9b0fb32ac25
mgrouch <mgrushinskiy@gmail.com>
2022-11-15 19:05:03 -0500

    vert_pos

--- imu/src/main.rs
+++ imu/src/main.rs
@@ -109,7 +109,7 @@ fn main() -> io::Result<()> {
 
         loop {
             match mpu9250.all() {
-                Ok(all: MargMeasurements<[f32; 3]>) => {
+                Ok(all) => {
                     // Obtain sensor values from a source
                     let gyroscope: Vector3<f64> = Vector3::new(all.gyro[0] as f64, all.gyro[1] as f64, all.gyro[2] as f64);
                     let accelerometer: Vector3<f64> = Vector3::new(all.accel[0] as f64, all.accel[1] as f64, all.accel[2] as f64);
