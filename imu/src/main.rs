1a3ca51fa6af46268cfba0d817c8d503ce7794e5
mgrouch <mgrushinskiy@gmail.com>
2022-11-15 23:56:00 -0500

    vert_pos

--- imu/src/main.rs
+++ imu/src/main.rs
@@ -37,7 +37,7 @@ fn main() -> io::Result<()> {
     let mag_who_am_i = mpu9250.ak8963_who_am_i()
         .expect("could not read magnetometer's WHO_AM_I");
     println!("who_am_I              |     0x{:x}", who_am_i);
-    println!("AK8963 who_am_I       |     0x{:x}", mag_who_am_i);
+    println!("ak8963 who_am_I       |     0x{:x}", mag_who_am_i);
     assert_eq!(who_am_i, 0x71);
 
     mpu9250.accel_data_rate(AccelDataRate::DlpfConf(Dlpf::_3)).expect("Err setting rate");
