e91cf57392f750b34264c93915e5371c44101e89
mgrouch <mgrushinskiy@gmail.com>
2022-11-16 22:16:32 -0500

    vert_pos

--- imu-python/imu-python.py
+++ imu-python/imu-python.py
@@ -77,7 +77,7 @@ P0 = [[PosIntegral_Variance, 0, 0],
       [0, 0, 0]]
 
 # Evaluate signal offset from 0
-Acc_Mean = 0.0  # TODO: calculate using running avg over long periods 1 mins or so.
+Acc_Mean = np.array(0.0)  # TODO: calculate using running avg over long periods 1 mins or so.
 
 filtered_state_means = X0
 filtered_state_covariances = P0
@@ -97,10 +97,10 @@ while True:
         accel = data["accel"]
         timestamp = data["timestamp"]
         timestamp_ms = timestamp / 1000
-        alignedAccel = np.asarray(quaternion.rotvecquat(accel, data['fusionQPose']))
+        alignedAccel = quaternion.rotvecquat(accel, data['fusionQPose'])
 
         observation = 0
-        acc = alignedAccel[2]
+        acc = np.array(alignedAccel[2])
         transition_offset = B * (acc - Acc_Mean)
         filtered_state_means, filtered_state_covariances = kf.filter_update(
             filtered_state_mean=filtered_state_means,
