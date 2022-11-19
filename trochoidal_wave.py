d3c1cd5f0fc34029803078964746ef769fda4ee5
mgrouch <mgrushinskiy@gmail.com>
2022-11-18 23:57:10 -0500

    Approx formula to estimate acceleration on top of wave

--- trochoidal_wave.py
+++ trochoidal_wave.py
@@ -16,6 +16,8 @@ T = L / c  # Wave period (s)
 
 # Approx formula to estimate acceleration on top of wave
 a_min_est = - np.exp(b * 2 * np.pi / L) / (1 - 5./3. * np.exp(b * 2 * np.pi / L))
+# or (reverse)
+# b_est = (L / (2 * np.pi)) * np.log(a_min / ((5.0 * a_min / 3.0) - 1)
 
 print(f'Length: {L}, Height: {H}, Period: {T}, Speed: {c}')
 
